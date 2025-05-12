87e977e369daa4403cadef1538854fbfddabbfd7
koayniaina <setraniaina08@gmail.com>
2025-05-12 21:44:27 +0300

    create page auth

---
+++ components/User.tsx
@@ -0,0 +1,31 @@
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+
+export default async function User() {
+  const supabase = createClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const signOut = async () => {
+    'use server';
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect('/login');
+  };
+
+  return (
+    session && (
+      <div>
+        Hey, {session.user.email}!
+        <form action={signOut}>
+          <button>
+            Logout
+          </button>
+        </form>
+      </div>
+    )
+  );
+}
